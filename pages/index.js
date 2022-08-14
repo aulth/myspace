2b82cabf03f8c7b9c26e3c53b18d26ec530c2b81
aulth <mohdusman.you@gmail.com>
2022-08-14 14:51:17 +0530

    vercel link added in getserversideprops

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({allItems, msg}) {
   )
 }
 export async function getServerSideProps(context) {
-      const response = await fetch('http://localhost:3000/api/getallitem');
+      const response = await fetch('https://myspacebook.vercel.app/api/getallitem');
       const data = await response.json();
       if(data.success){
           return {
